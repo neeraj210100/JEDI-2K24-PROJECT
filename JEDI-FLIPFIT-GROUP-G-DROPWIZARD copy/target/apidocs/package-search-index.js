23b04ffecd74600c5cf3dae26525c9ce845e7f9a
neeraj210100 <neerajsharmaa2101@gmail.com>
2024-11-12 17:05:49 +0530

    final project setup

--- JEDI-FLIPFIT-GROUP-G-DROPWIZARD copy/target/apidocs/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"com.flipfit.app"},{"l":"com.flipfit.bean"},{"l":"com.flipfit.business"},{"l":"com.flipfit.business.interfaces"},{"l":"com.flipfit.constant"},{"l":"com.flipfit.dao"},{"l":"com.flipfit.dao.interfaces"},{"l":"com.flipfit.exceptions"},{"l":"com.flipfit.restController"},{"l":"com.flipfit.utils"}]
